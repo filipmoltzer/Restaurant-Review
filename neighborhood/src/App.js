f4c8d42f6e733efcff363082f60ceda95f65297f
Filip-Moltzer <filip.moltzer@gmail.com>
2018-08-27 13:31:40 +0200

    + Added venues from third-part FourSquare

--- neighborhood/src/App.js
+++ neighborhood/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import axios from 'axios';
 import React, { Component } from 'react';
 import logo from './logo.svg';
 
 class App extends Component {
 
+  state = {
+    venues: []
+  }
+
   componentDidMount() { //react componnent mount=we invoke the renderMap function
     this.renderMap()
+    this.getVenues()
   }
 
   renderMap = () => {
@@ -13,11 +19,42 @@ class App extends Component {
     window.initMap = this.initMap
   }
 
+    // foursquare
+    getVenues = () => {
+      const endPoint = "https://api.foursquare.com/v2/venues/explore?"
+      const parameters = {
+        client_id: "5FYYDPU5J1B2T45JT4B1P5OFEGA4ZSPWARV5ZIZJKJUJLFGM",
+        client_secret: "R1KLNCE1UOR5Z5GA43Y2CGAKCS0WMZDBRJEOP0DOCTM3HK4W",
+        query: "food",
+        near: "Sydney",
+        v: "20182507"
+      }
+
+    // axios
+    axios.get(endPoint + new URLSearchParams(parameters))
+    .then(response => {
+      this.setState ({
+        venues: response.data.response.groups[0].items
+      })
+    })
+    .catch(error => {
+      console.log("Ooops, Error details: " + error)
+    })
+
+  } //foursquare end-bracket
+
+
+
   initMap = () => {
         var map = new window.google.maps.Map(document.getElementById('map'), {
           center: {lat: -34.397, lng: 150.644},
           zoom: 8
         })
+        var marker = new window.google.maps.Marker({
+          position: {lat: -34.397, lng: 150.644},
+          map: map,
+          title: 'Hello World!'
+      });
       }
 
   render() {
