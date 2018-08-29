b9be87d78c3d53bf109e2c5cfb3a5ad76c1e8ce3
filipmoltzer <40041318+filipmoltzer@users.noreply.github.com>
2018-08-29 18:55:18 +0200

    Whole file uploaded bc git lag

---
+++ malmo/README.md
@@ -0,0 +1,32 @@
+# **Neighbourhood MAP App Project**
+# Udacity FEND Project 8
+
+### **Definition**
+This is a project built using mainly React.
+It has one main page that shows you the Top 5 best attractions around Malmö, Sweden using Google maps API.
+To access an information window about the specific place you can do one of two things:
+Either press one of the Map markers on the Google Map,
+Or use the navbar list that links to the specific location of your choice.
+
+
+
+### **Install:**
+**Step 1.**
+Clone/Download this project.
+
+**Step 2.**
+Cd into 'Malmo-Top5-Map' folder in Terminal.
+run: npm install
+
+**Start the application:**
+run: npm start
+The app will run locally on port 3000 (http://localhost:3000/).
+
+#### **Service Worker**
+The service worker only runs in product mode.
+To start production mode you need to run 'npm run build' then 'serve -s build'
+
+
+##### **Additional**
+This was a project from Udacity FEND nanodegree.
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
---
+++ malmo/src/navbar.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+class Nav extends React.Component {
+    render() {
+        const { locations, query, filter } = this.props
+        const links = locations.map((location) => (
+
+          /* Here I used alot of building/styling with Bootstrap.
+              + Navbar & Grid layouts are buildt within the Bootstrap grid system.
+              + Background color, collapse menu are also buildt through Bootstrap.
+            */
+        <button key={location.id} onClick={e => this.props.onLocClicked(e.target)} className="list-group-item list-group-item-action flex-column align-items-startmy-1">
+            <div className="d-flex w-100 justify-content-between">
+                <h5>{location.name}</h5>
+            </div>
+        </button>
+        ))
+
+        return (<div className="col-lg-3 p-0">
+            <nav className="side-nav navbar navbar-expand-lg navbar-dark bg-info flex-lg-column">
+                <h1 className="navbar-brand text-center ">Malmö Sightseeing <br/> Top 5</h1><button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse flex-column w-100" id="navbarSupportedContent">
+                    <div className="input-group my-3">
+                        <input className="form-control mr-sm-2" type="search" value={query}
+                            onChange={(event) => filter(event.target.value)}
+                            placeholder="Search..." aria-label="Search"></input>
+
+                    </div>
+                    <div className="list-group p-0 mt-3 w-1000">
+                        <ReactCSSTransitionGroup
+                            transitionName={{
+                                enter: 'animated',
+                                enterActive: 'slideInDown',
+                                leave: 'animated',
+                                leaveActive: 'fadeOut',
+                                appear: 'animated ',
+                                appearActve: 'slideInDown'
+                            }} transitionEnterTimeout={1000}
+                            transitionLeaveTimeout={500}
+                            transitionAppearTimeout={800}
+                            transitionEnter={false} // Fixes console warning error
+                            >
+                            {links}
+                        </ReactCSSTransitionGroup>
+                    </div>
+                    <div className="input-group-append p-3">
+                        <button className="btn btn-warning" type="button" onClick={this.props.clear}>Back</button>
+                    </div>
+                </div>
+            </nav>
+        </div >
+        )
+    }
+}
+export default Nav;
